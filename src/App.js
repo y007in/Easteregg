267d96c59de76309019a17424e82ebdb0db6b49f
DESKTOP-R2R34G5\Administrator <ujin4719@naver.com>
2022-10-11 23:05:30 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./data.json";
 
 function App() {
+  const getRandomIndex = function (length) {
+    return parseInt(Math.random() * length);
+  };
+
+  const reload = function reloadPage() {
+    location.reload();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head_text">
+        <img src={`${process.env.PUBLIC_URL}img/x.png`} />
+        <h2>인터넷에 연결되어 있지 않아요.</h2>
+        <h4>ERR_INTERNET_DISCONNECTED</h4>
+      </div>
+      <div className="content_wrap">
+        <div className="video">
+          <video src="/whale.mp4" loop autoPlay muted />
+        </div>
+        <div className="text_balloon">
+          <img src={`${process.env.PUBLIC_URL}img/text_balloon.png`} />
+          <button type="button" onClick={reload}>
+            <img src={`${process.env.PUBLIC_URL}img/reload.png`} />
+          </button>
+          <div className="text">{data[getRandomIndex(data.length)].text}</div>
+        </div>
+      </div>
     </div>
   );
 }
