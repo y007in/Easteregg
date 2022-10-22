7f765c897a632cae9ca88cd38b9dad7a1c8bb072
DESKTOP-R2R34G5\Administrator <ujin4719@naver.com>
2022-10-22 16:59:08 +0900

    FIX:코드 수정(피드백 반영)

--- src/App.js
+++ src/App.js
@@ -14,19 +14,23 @@ function App() {
   return (
     <div className="App">
       <div className="head_text">
-        <img src={`${process.env.PUBLIC_URL}img/x.png`} />
+        <img src={`${process.env.PUBLIC_URL}img/xx.png`} />
+        <h2>인터넷에 연결되어있지 않아요.</h2>
+        <h4>ERR_INTERNET_DISCONNECTED</h4>
       </div>
       <div className="content_wrap">
         <div className="video">
-          <video src="/whale.mp4" loop autoPlay muted />
+          <video src="/whale.mp4" loop autoPlay muted alt="whale dance" />
         </div>
         <div className="text_balloon">
-          <img src={`${process.env.PUBLIC_URL}img/text_balloon.png`} />
-          <button type="button" onClick={reload}>
-            <img src={`${process.env.PUBLIC_URL}img/reload.png`} />
-          </button>
           <div className="text">{data[getRandomIndex(data.length)].text}</div>
         </div>
+        <button type="button" onClick={reload}>
+          <img
+            src={`${process.env.PUBLIC_URL}img/reload.png`}
+            alt="reload button"
+          />
+        </button>
       </div>
     </div>
   );
